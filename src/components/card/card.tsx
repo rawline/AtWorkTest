068e45fa85abf8662f9780fb715b2edc06cb15a5
Rafael Mamatkhodjaev <rawoline@gmail.com>
2024-09-17 16:32:34 +0300

    some changes

--- src/components/card/card.tsx
+++ src/components/card/card.tsx
@@ -20,6 +20,18 @@ const Card: React.FC<CardProps> = ({ card, onArchive, onActive }) => {
         [styles.grayscale]: card.archived, // Добавляем класс для черно-белого изображения, если карточка архивирована
     });
 
+    const title = classNames(styles.title, {
+        [styles.archivedCardTitle]: card.archived,
+    });
+
+    const subTitle = classNames(styles.subTitle, {
+        [styles.archivedSubTitle]: card.archived,
+    });
+
+    const city = classNames(styles.cardCity, {
+        [styles.archivedCity]: card.archived,
+    });
+
     const dropDown = classNames(styles.dropdown, {
         [styles.dropdownArchived]: card.archived,
     });
@@ -37,7 +49,7 @@ const Card: React.FC<CardProps> = ({ card, onArchive, onActive }) => {
 
             <div className={styles.textBlock}>
                 <div className={styles.cardTitle}>
-                    <h3 className={styles.title}>{card.name}</h3>
+                    <h3 className={title}>{card.name}</h3>
                     <svg
                         viewBox="0 0 4 17"
                         xmlns="http://www.w3.org/2000/svg"
@@ -47,8 +59,8 @@ const Card: React.FC<CardProps> = ({ card, onArchive, onActive }) => {
                         <path d="M0 14.5C0 15.6 0.9 16.5 2 16.5C3.1 16.5 4 15.6 4 14.5C4 13.4 3.1 12.5 2 12.5C0.9 12.5 0 13.4 0 14.5ZM0 2.5C0 3.6 0.9 4.5 2 4.5C3.1 4.5 4 3.6 4 2.5C4 1.4 3.1 0.5 2 0.5C0.9 0.5 0 1.4 0 2.5ZM0 8.5C0 9.6 0.9 10.5 2 10.5C3.1 10.5 4 9.6 4 8.5C4 7.4 3.1 6.5 2 6.5C0.9 6.5 0 7.4 0 8.5Z" />
                     </svg>
                 </div>
-                <p className={styles.subTitle}>{card.companyName}</p>
-                <p className={styles.cardCity}>{card.city}</p>
+                <p className={subTitle}>{card.companyName}</p>
+                <p className={city}>{card.city}</p>
             </div>
 
             {menuOpen && !card.archived && (
--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,14 +1,18 @@
 import styles from './header.module.scss'
+import logo from '../../../public/logo.svg'
+import fav from "../../../public/Favorite.svg"
+import not from "../../../public/Notification.svg"
+import photo from "../../../public/Foto.png"
 
 export const Header = () => {
     return (
         <div className={styles.header}>
             <div className={styles.container}>
-                <img src="../../../public/logo.svg" alt="logo" />
+                <img src={logo} alt="logo" />
                 <div className={styles.menu}>
-                    <img className={styles.icons} src="../../../public/Favorite.svg" alt="Favourite" />
-                    <img className={styles.icons} src="../../../public/Notification.svg" alt="Notification" />
-                    <img className={styles.photo} src="../../../public/Foto.png" alt="foto" />
+                    <img className={styles.icons} src={fav} alt="Favourite" />
+                    <img className={styles.icons} src={not} alt="Notification" />
+                    <img className={styles.photo} src={photo} alt="foto" />
                     <p className={styles.name}>ivan123</p>
                 </div>
             </div>
